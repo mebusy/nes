bd9cba1cec35ff074e0910eb00b2456f74c5c7a7
qibinyi <qibinyi@qibinyis-imac.ubisoft.org>
2018-09-12 18:14:56 +0800

    m

--- src/nes/cheater/cheater.go
+++ src/nes/cheater/cheater.go
@@ -7,6 +7,8 @@ import (
 	_ "io/ioutil"
 	"log"
 	"net/http"
+	"reflect"
+	"strconv"
 	"sync"
 )
 
@@ -19,21 +21,81 @@ func clearMap(ram []byte) {
 	}
 	ramMap.Range(delete)
 
-	for i, v := range ram {
-		// log.Println(i, v)
-		key := fmt.Sprintf("%d:%d", i, 1)
-		ramMap.Store(key, v)
+	for addr, v := range ram {
+		//fmt.Println(addr, v)
+		key := fmt.Sprintf("%d:%d", addr, 1)
+		store2map(key, v)
 
-		if i < len(ram)-1 {
-			key = fmt.Sprintf("%d:%d", i, 2)
-			ramMap.Store(key, binary.LittleEndian.Uint16(ram[i:]))
+		if addr <= len(ram)-2 {
+			key = fmt.Sprintf("%d:%d", addr, 2)
+			store2map(key, binary.LittleEndian.Uint16(ram[addr:]))
 		}
-		if i < len(ram)-3 {
-			key = fmt.Sprintf("%d:%d", i, 4)
-			ramMap.Store(key, binary.LittleEndian.Uint32(ram[i:]))
+		if addr <= len(ram)-4 {
+			key = fmt.Sprintf("%d:%d", addr, 4)
+			store2map(key, binary.LittleEndian.Uint32(ram[addr:]))
 		}
 
 	}
+
+}
+
+func mapCount() int {
+	cnt := 0
+	count := func(key interface{}, value interface{}) bool {
+		cnt++
+		return true
+	}
+	ramMap.Range(count)
+	return cnt
+}
+
+func interface2uint32(val interface{}) uint32 {
+	switch val.(type) {
+	case int:
+		return uint32(val.(int))
+	case uint8:
+		return uint32(val.(uint8))
+	case uint16:
+		return uint32(val.(uint16))
+	case uint32:
+		return val.(uint32)
+	default:
+		fmt.Println(reflect.ValueOf(val).Kind())
+		panic("不知道什么整数类型")
+	}
+
+}
+
+func store2map(key string, val interface{}) {
+	ramMap.Store(key, interface2uint32(val))
+}
+
+func debugInfo(w http.ResponseWriter, ram []byte) {
+	//io.WriteString(w, fmt.Sprintf("val of addr 0: %d", ram[0]))
+}
+
+func print10matchedAddress(w http.ResponseWriter, ram []byte) {
+	cnt := 0
+	dump := func(_key interface{}, _value interface{}) bool {
+		addr := 0
+		nByte := 0
+		key := _key.(string)
+		fmt.Sscanf(key, "%d:%d", &addr, &nByte)
+
+		cnt++
+		if cnt > 10 {
+			return false
+		}
+
+		io.WriteString(w, fmt.Sprintf("%04X : %12d , ", addr, _value.(uint32)))
+		for i := 0; i < nByte; i++ {
+			io.WriteString(w, fmt.Sprintf("%02x ", ram[addr+i]))
+		}
+		io.WriteString(w, "\n")
+
+		return true
+	}
+	ramMap.Range(dump)
 
 }
 
@@ -43,11 +105,85 @@ func search(ram []byte, w http.ResponseWriter, req *http.Request) {
 	if ok && len(vals[0]) >= 1 {
 		// Query()["val"] will return an array of items,
 		// we only want the single item.
-		val := vals[0]
+		val, _ := strconv.Atoi(vals[0])
 
-		// only compare current value  , and compared value , no stored value
-		io.WriteString(w, "val: ")
-		io.WriteString(w, val)
+		// only compare current value  with input value , no stored value
+		equal := func(val1, val2 interface{}) bool {
+			v1 := interface2uint32(val1)
+			v2 := interface2uint32(val2)
+			return v1 == v2
+		}
+		comparator := equal
+
+		valueCompare := func(_key interface{}, _ interface{}) bool {
+			addr := 0
+			nByte := 0
+			key := _key.(string)
+			fmt.Sscanf(key, "%d:%d", &addr, &nByte)
+
+			if nByte == 1 && comparator(val, ram[addr]) {
+				// store new value
+				store2map(key, ram[addr])
+
+			} else if nByte == 2 && comparator(val, binary.LittleEndian.Uint16(ram[addr:])) {
+				store2map(key, binary.LittleEndian.Uint16(ram[addr:]))
+			} else if nByte == 4 && comparator(val, binary.LittleEndian.Uint32(ram[addr:])) {
+				store2map(key, binary.LittleEndian.Uint32(ram[addr:]))
+			} else {
+				ramMap.Delete(key)
+			}
+
+			return true
+		}
+		ramMap.Range(valueCompare)
+
+		io.WriteString(w, "matched result: ")
+		io.WriteString(w, fmt.Sprintf("%d\n", mapCount()))
+		debugInfo(w, ram)
+
+		return
+	}
+
+	// compare stored value and current value
+	vals, ok = req.URL.Query()["diff"]
+	if ok && len(vals[0]) >= 1 {
+		op := vals[0]
+		_ = op
+
+		lessThan := func(val1, val2 interface{}) bool {
+			v1 := interface2uint32(val1)
+			v2 := interface2uint32(val2)
+			return v1 < v2
+		}
+		comparator := lessThan
+
+		valueCompare := func(_key interface{}, _ interface{}) bool {
+			addr := 0
+			nByte := 0
+			key := _key.(string)
+			fmt.Sscanf(key, "%d:%d", &addr, &nByte)
+
+			_val, ok2 := ramMap.Load(key)
+			val := _val.(uint32)
+
+			if nByte == 1 && ok2 && comparator(val, ram[addr]) {
+				// store new value
+				store2map(key, ram[addr])
+			} else if nByte == 2 && comparator(val, binary.LittleEndian.Uint16(ram[addr:])) {
+				store2map(key, binary.LittleEndian.Uint16(ram[addr:]))
+			} else if nByte == 4 && comparator(val, binary.LittleEndian.Uint32(ram[addr:])) {
+				store2map(key, binary.LittleEndian.Uint32(ram[addr:]))
+			} else {
+				ramMap.Delete(key)
+			}
+
+			return true
+		}
+		ramMap.Range(valueCompare)
+
+		io.WriteString(w, "matched result: ")
+		io.WriteString(w, fmt.Sprintf("%d", mapCount()))
+
 		return
 	}
 
@@ -61,16 +197,19 @@ func StartWebServer(ram []byte) {
 	go func() {
 		helloHandler := func(w http.ResponseWriter, req *http.Request) {
 			if req.URL.Path == "/clear" {
-				io.WriteString(w, "clear!\n")
 				clearMap(ram)
+				io.WriteString(w, "clear!\n")
+				debugInfo(w, ram)
 			} else if req.URL.Path == "/search" {
 				search(ram, w, req)
-
 			} else {
 				io.WriteString(w, "Example\n")
 				io.WriteString(w, "/clear\n")
 				io.WriteString(w, "/search?val=12\n")
 			}
+
+			//
+			print10matchedAddress(w, ram)
 		}
 
 		http.HandleFunc("/", helloHandler)
